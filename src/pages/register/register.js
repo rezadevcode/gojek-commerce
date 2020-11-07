7eb47a474d48073c7b107561186c7d487a9bcb5c
akbar_rahadian <akbar@glovory.com>
2020-11-07 07:58:02 +0700

    first commit - adding login page & signup page

---
+++ src/pages/register/register.js
@@ -0,0 +1,41 @@
+/** LOGIN SCRIPT */
+
+$(document).ready(function () {
+
+    $(".selection").select2({
+        minimumResultsForSearch: Infinity,
+    });
+
+    // hide default select2 arrow down
+    $('b[role="presentation"]').hide();
+
+    // adjust arrow icon when select2 opened
+    $('.selection').on('select2:open', function (e) {
+        const icon = $(this).parents('.form-group').find('.fas');
+        icon.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+    });
+    $('.selection').on('select2:close', function (e) {
+        const icon = $(this).parents('.form-group').find('.fas');
+        icon.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+    });
+
+    // config jquery form validate
+    $("#loginForm").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            fullname: {
+                required: true,
+                minlength: 3,
+                maxlength: 100
+            }
+        },
+        submitHandler: function(form) {
+            alert('form submitted');
+            // form.submit();
+        }
+    });
+
+});
