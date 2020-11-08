203bb90b518b33a0570436605143c0440c76c49e
akbar_rahadian <akbar@glovory.com>
2020-11-09 05:02:14 +0700

    🛠 [Update] Product List

---
+++ src/pages/product/list/specific_category/script.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function () {
+
+    $(".selection").select2({
+        minimumResultsForSearch: Infinity,
+    });
+
+    // hide default select2 arrow down
+    $('b[role="presentation"]').hide();
+});
