9b7cdd8054a26c1d1cebac0ab14ddb919ed1e4e3
akbar_rahadian <akbar@glovory.com>
2020-11-10 21:41:56 +0700

    🛠 [UPDATE] Spesific Category responsiveness column

--- src/pages/product/list/specific_category/script.js
+++ src/pages/product/list/specific_category/script.js
@@ -1,10 +1,23 @@
 
 
 $(document).ready(function () {
+    const windowsize = $(window).width();
 
     $(".selection").select2({
-        minimumResultsForSearch: Infinity,
+        minimumResultsForSearch: Infinity
     });
+
+
+    if (windowsize < 768) {
+        $("#sorting").select2({
+            minimumResultsForSearch: Infinity,
+            placeholder: "Sorting"
+        });
+        $("#menu-category").select2({
+            minimumResultsForSearch: Infinity,
+            placeholder: "All Gadget"
+        });
+    }
 
     // hide default select2 arrow down
     $('b[role="presentation"]').hide();
